478921c2866a708b1fe7dd3fde036cf4a6353feb
onlucscrin <vinutha.m.reddy@gmail.com>
2021-07-25 08:27:53 -0400

    Initial version develop branch

---
+++ src/main/java/org/datastructure/linkedlist/LinkedlistApplication.java
@@ -0,0 +1,13 @@
+package org.datastructure.linkedlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkedlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkedlistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/datastructure/linkedlist/LinkedlistApplicationTests.java
@@ -0,0 +1,13 @@
+package org.datastructure.linkedlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkedlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
